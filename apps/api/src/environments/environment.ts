5f89b821a67b86fa404f7a15a3597748d98c5da2
Oyuka <oyuka579@gmail.com>
2022-07-01 14:37:52 +0800

    Delete apps/api/src/environments directory

--- apps/api/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  clientId: '5a9a0999b877481f6e49',
-  appUrl: 'http://localhost:4200/',
-  clientSecret: '3b974a5cad811a542505a740bddaef8fd70d6073',
-  appSecret: 'wMX4QtbNuJWygKrXZqXp4y6wyrRPsxo3w0cXyqghHnc=',
-  connectionString: 's'
-};
